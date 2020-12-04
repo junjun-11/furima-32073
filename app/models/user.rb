a8f6a5cffa51d22c10832b4a9c078cbb6f536401
junjun-11 <exceed-the-limit.phiten@hotmail.co.jp>
2020-12-04 18:55:52 +0900

    エラー表示,全角,全角カナバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :furigana_last, presence: true
-  validates :furigana_first, presence: true
+   validates :nickname, presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Full-width characters' } do
+   validates :last_name
+   validates :first_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: 'Full-width katakana characters' } do
+   validates :furigana_last
+   validates :furigana_first
+  end
   validates :date_of_birth, presence: true
        
 end
