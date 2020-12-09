11c66f7dc865dbfea0696ef7e2f78201e0b5d955
junjun-11 <exceed-the-limit.phiten@hotmail.co.jp>
2020-12-09 14:40:40 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,17 @@ class Item < ApplicationRecord
   belongs_to :delivery_fee
   belongs_to :prefecture
   belongs_to :day_to_ship
-  
+
+  validates :item_name, presence: true
+  validates :explanation, presence: true
+
+  validates :category_id, numericality: { other_than: 1 } 
+  validates :status_id, numericality: { other_than: 1 } 
+  validates :delivery_fee_id, numericality: { other_than: 1 } 
+  validates :prefecture_id, numericality: { other_than: 1 } 
+  validates :day_to_ship_id, numericality: { other_than: 1 } 
+
+  validates :selling_price, presence: true
 
   validates :image, presence: true
 
