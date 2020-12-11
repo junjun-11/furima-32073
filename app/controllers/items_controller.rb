e83a8689c20f9f00617b9ededda8b52a34ec4b20
junjun-11 <exceed-the-limit.phiten@hotmail.co.jp>
2020-12-11 15:51:18 +0900

    ログイン状態の出品者だけが商品情報編集ページに遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :move_to_index, only: :edit
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -21,9 +22,19 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:item_name, :explanation, :category_id, :status_id, :delivery_fee_id, :prefecture_id, :day_to_ship_id, :selling_price, :image).merge(user_id: current_user.id)
+  end
+end
+
+def move_to_index
+  @item = Item.find(params[:id])
+  unless user_signed_in? && current_user.id == @item.user.id
+    redirect_to action: :index
   end
 end
